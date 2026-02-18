10cac3da1b87449601054c45ba89180c44efb82f
Adonis Almonte <dev.adonis.almonte@gmail.com>
2026-02-17 22:16:03 -0500

    Bug Fix: Solve an exception where was not able to display multiple scrolls at once

--- lib/widgets/transaction_item.dart
+++ lib/widgets/transaction_item.dart
@@ -16,6 +16,13 @@ class TransactionItem extends StatefulWidget {
 
 class _TransactionItemState extends State<TransactionItem> {
   bool _expanded = false;
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,18 +93,21 @@ class _TransactionItemState extends State<TransactionItem> {
             maxHeight: _expanded ? min(74.0 * transactions.length, 74 * 3) : 0,
           ),
           padding: const EdgeInsets.only(left: 16, right: 16, bottom: 8),
-          child: Scrollbar(
-            thumbVisibility: true,
-            trackVisibility: true,
-            radius: const Radius.circular(20),
-            child: ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (context, index) => TransactionTile(
-                transactionType: transactions[index].type,
-                category: transactions[index].category,
-                amount: transactions[index].amount,
-                date: transactions[index].date,
-                isLastItem: index == transactions.length - 1,
+          child: PrimaryScrollController(
+            controller: _scrollController,
+            child: Scrollbar(
+              thumbVisibility: true,
+              trackVisibility: true,
+              radius: const Radius.circular(20),
+              child: ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (context, index) => TransactionTile(
+                  transactionType: transactions[index].type,
+                  category: transactions[index].category,
+                  amount: transactions[index].amount,
+                  date: transactions[index].date,
+                  isLastItem: index == transactions.length - 1,
+                ),
               ),
             ),
           ),
